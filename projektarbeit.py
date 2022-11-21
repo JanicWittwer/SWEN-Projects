6509f2351bd6a43fb5b1c11eff3707858e5ecace
JanicWittwer <92203361+JanicWittwer@users.noreply.github.com>
2022-11-21 10:55:13 +0100

    Update projektarbeit.py

--- projektarbeit.py
+++ projektarbeit.py
@@ -1,25 +1,39 @@
 # Das ist unsere Hauptdatei für die Projektarbeit
 
-#Test ob es funktioniert
+## Würfeln-Funktion mit 6 Würfeln
+### Erste Funktion, welche wir umgesetzt haben. Hier programmieren wir , dass ein einzelner Spieler die Funktion hat, mit 6 Würfeln immer wieder zu würfeln. Mittel Ja und Nein Input-Befehlen kann er sagen, ob er erneut würfeln möchte. Es sind noch keine Anzahl Spieler, keine Punkte, keine Regelen und keine Spiellogik implementiert.
 
-#Test Milo
 
 import random
 import time
 
 Playing = True
 
-while playing:
-    whileTrue:
+while Playing:
+    while True:
 
-        janein = input("Möchtest du die Würfel würfeln? j/n")
+        janein = input("Möchtest du die Würfel würfeln? j/n\n>")
         if janein == "j":
-            print("Dann lasst uns beginnen")
-            Augen = random.randint(1, 6)
-            print("Das Ergebnis ist:" + str(Augen))
+            print("Dann lass uns beginnen!")
+            wuerfel1 = random.randint(1, 6)
+            wuerfel2 = random.randint(1, 6)
+            wuerfel3 = random.randint(1, 6)
+            wuerfel4 = random.randint(1, 6)
+            wuerfel5 = random.randint(1, 6)
+            wuerfel6 = random.randint(1, 6)
+            print("Der Würfel zeigt: " + str(wuerfel1))
+            print("Der Würfel zeigt: " + str(wuerfel2))
+            print("Der Würfel zeigt: " + str(wuerfel3))
+            print("Der Würfel zeigt: " + str(wuerfel4))
+            print("Der Würfel zeigt: " + str(wuerfel5))
+            print("Der Würfel zeigt: " + str(wuerfel6))
         elif janein == "n":
-            pirint ("Oka, dann nicht...")
+            print ("Okay, dann schreiben wir deine Punkte auf...")
             quit()
         else: 
-        print("Bitte schreib etwas sinnvolles. (j/n")
+            print("Bitte schreib etwas sinnvolles. (j/n")
+
+## Anzahl Spieler und Spielernamen
+### Wir möchten nun eine Funktion einbauen, wo am Anfang eine Anzahl Spieler gewählt werden kann und auch Spielernamen eingegeben werden können.
+
         
