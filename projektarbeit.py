8d79b7e527fe0c91b149fb2f4cefa4073a1cf858
dariojnglin <91521853+dariojnglin@users.noreply.github.com>
2022-11-21 10:20:05 +0100

    Update projektarbeit.py

--- projektarbeit.py
+++ projektarbeit.py
@@ -3,3 +3,23 @@
 #Test ob es funktioniert
 
 #Test Milo
+
+import random
+import time
+
+Playing = True
+
+while playing:
+    whileTrue:
+
+        janein = input("Möchtest du die Würfel würfeln? j/n")
+        if janein == "j":
+            print("Dann lasst uns beginnen")
+            Augen = random.randint(1, 6)
+            print("Das Ergebnis ist:" + str(Augen))
+        elif janein == "n":
+            pirint ("Oka, dann nicht...")
+            quit()
+        else: 
+        print("Bitte schreib etwas sinnvolles. (j/n")
+        
