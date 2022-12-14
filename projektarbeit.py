1647bb2ba0a2761daa869a4c5051c54fd4520d2a
JanicWittwer <92203361+JanicWittwer@users.noreply.github.com>
2022-12-14 20:10:41 +0100

    finale_version.py

--- projektarbeit.py
+++ projektarbeit.py
@@ -1,143 +1,11 @@
-# Das ist unsere Hauptdatei für die Projektarbeit
 
-## Teil 1: Anzahl Spieler und Spielernamen
-### Wir möchten nun eine Funktion einbauen, wo am Anfang eine Anzahl Spieler gewählt werden kann und auch Spielernamen eingegeben werden können.
-
-
-print("Herzlich willkommen zu Fraggles - Das würfligste Würfelspiel für Gross und Klein. Bei diesem Spiel gehen Freundschaften zu Bruch.")
-
-def spieler_eingabe():
-    while True:
-        anzahl_spieler = input("Bitte Anzahl Spieler eingeben: ")    
-        if anzahl_spieler >= "2" and anzahl_spieler <= "6" :
-            return int(anzahl_spieler)
-        else:
-            print("Die Anzahl Spieler muss zwischen 2 und 6 liegen")
-
-anzahl_spieler = spieler_eingabe()
-print(anzahl_spieler)
-
-
-class Spieler:
-    def __init__(self, name):
-        self.name = name
-        self.punktestand_temp = 0
-        self.punktestand_total = 0
-
-spieler = []
-
-for i in range(anzahl_spieler):
-    neuer_spieler = Spieler(input(f"Bitte gib den Namen von Spieler {i+1} ein: "))
-    spieler.append(neuer_spieler)
-
-    
-
-## Teil 2: Würfeln-Funktion mit 6 Würfeln
-### Erste Funktion, welche wir umgesetzt haben. Hier programmieren wir , dass ein einzelner Spieler die Funktion hat, mit 6 Würfeln immer wieder zu würfeln. Mittel Ja und Nein Input-Befehlen kann er sagen, ob er erneut würfeln möchte. Es sind noch keine Anzahl Spieler, keine Punkte, keine Regelen und keine Spiellogik implementiert.
-
+# Imports
 
 import random
-import time
-
-Playing = True
-
-while Playing:
-    while True:
-
-        janein = input("Möchtest du die Würfel würfeln? j/n\n>")
-        if janein == "j":
-            print("Dann lass uns beginnen!")
-            wuerfel1 = random.randint(1, 6)
-            wuerfel2 = random.randint(1, 6)
-            wuerfel3 = random.randint(1, 6)
-            wuerfel4 = random.randint(1, 6)
-            wuerfel5 = random.randint(1, 6)
-            wuerfel6 = random.randint(1, 6)
-            print("Der Würfel zeigt: " + str(wuerfel1))
-            print("Der Würfel zeigt: " + str(wuerfel2))
-            print("Der Würfel zeigt: " + str(wuerfel3))
-            print("Der Würfel zeigt: " + str(wuerfel4))
-            print("Der Würfel zeigt: " + str(wuerfel5))
-            print("Der Würfel zeigt: " + str(wuerfel6))
-        elif janein == "n":
-            print ("Okay, dann schreiben wir deine Punkte auf...")
-            quit()
-
-            
-    
-## Interpretation des Würfelwurfs anhand der Werte/Regeln
-### Als nächstes möchten wir prüfen, ob der Würfelwurf Werte enthält, welche auch tatsächlich Punkte ergeben. Dazu haben wir verschiedene Regeln programmiert, welche von unserem Programm geprüft werden.   
-### Die Würfelfunktion aus Teil 2 wurde hier ebenfalls mitgenommen.    
-
-import random
-import time
-
-Playing = True
-
-while Playing:
-    while True:
-
-        janein = input("Möchtest du die Würfel würfeln? j/n\n>")
-        if janein == "j":
-            print("Dann lass uns beginnen!")
-            wuerfel1 = random.randint(1, 6)
-            wuerfel2 = random.randint(1, 6)
-            wuerfel3 = random.randint(1, 6)
-            wuerfel4 = random.randint(1, 6)
-            wuerfel5 = random.randint(1, 6)
-            wuerfel6 = random.randint(1, 6)
-            print("Der Würfel zeigt: " + str(wuerfel1))
-            print("Der Würfel zeigt: " + str(wuerfel2))
-            print("Der Würfel zeigt: " + str(wuerfel3))
-            print("Der Würfel zeigt: " + str(wuerfel4))
-            print("Der Würfel zeigt: " + str(wuerfel5))
-            print("Der Würfel zeigt: " + str(wuerfel6))
-
-            
-            wuerfelliste = [str(wuerfel1), str(wuerfel2), str(wuerfel3), str(wuerfel4), str(wuerfel5), str(wuerfel6)]
-            print(wuerfelliste)
-            anzahl_eins = wuerfelliste.count(str(1))
-            anzahl_zwei = wuerfelliste.count(str(2))
-            anzahl_drei = wuerfelliste.count(str(3))
-            anzahl_vier = wuerfelliste.count(str(4))
-            anzahl_fuenf = wuerfelliste.count(str(5))
-            anzahl_sechs = wuerfelliste.count(str(6))
-
-            kombinationen = [anzahl_eins, anzahl_zwei, anzahl_drei, anzahl_vier, anzahl_fuenf, anzahl_sechs]
-
-            strasse = [str(1),str(2), str(3), str(4), str(5), str(6)]
-
-            for i in wuerfelliste:
-                if str(1) in i:
-                    print("Super, du hast eine 1 gewürfelt, das gibt Punkte")
-                
-                elif str(5) in i:
-                    print("Super, du hast eine 5 gewürfelt, das gibt Punkte")
-                
-            for i in kombinationen:
-                if i >= 3:
-                    print("Super, du hast eine Dreierkombination gewürfelt, das gibt Punkte")
-
-
-        elif janein == "n":
-            print ("Okay, dann schreiben wir deine Punkte auf...")
-            quit()
-            
-
-            
-            
-            
-#Kombinierte Version
-###(Entwurf, aber schon einiges reinkopiert)
-
-
-#Imports
-
-import random
-import time
 
 # Definition von unseren zentralen Funktionen für das Spiel
 
+## Funktion Spielereingabe
 def spieler_eingabe():
     while True:
         anzahl_spieler = input("Bitte Anzahl Spieler eingeben: ___")    
@@ -146,168 +14,7 @@ def spieler_eingabe():
         else:
             print("Die Anzahl Spieler muss zwischen 2 und 6 liegen")
 
-
-# Nun definieren wir unsere Standard-Würfelfunktion für 6 Würfel
-
-def wuerfeln():
-    janein = input("Möchtest du die Würfel würfeln? j/n\n>")
-    if janein == "j":
-        print("Dann lass uns beginnen!")
-        wuerfel1 = random.randint(1, 6)
-        wuerfel2 = random.randint(1, 6)
-        wuerfel3 = random.randint(1, 6)
-        wuerfel4 = random.randint(1, 6)
-        wuerfel5 = random.randint(1, 6)
-        wuerfel6 = random.randint(1, 6)
-        print(f"Der Würfel zeigt: {wuerfel1}")
-        print(f"Der Würfel zeigt: {wuerfel2}")
-        print(f"Der Würfel zeigt: {wuerfel3}")
-        print(f"Der Würfel zeigt: {wuerfel4}")
-        print(f"Der Würfel zeigt: {wuerfel5}")
-        print(f"Der Würfel zeigt: {wuerfel6}")
-
-        wuerfelliste = [wuerfel1, wuerfel2, wuerfel3, wuerfel4, wuerfel5, wuerfel6]
-        print(wuerfelliste)
-        return wuerfelliste
-
-
-
-def hatpunkte(wuerfelliste):
-    hatpunkte = False
-    
-    anzahl_eins = wuerfelliste.count(1)
-    anzahl_zwei = wuerfelliste.count(2)
-    anzahl_drei = wuerfelliste.count(3)
-    anzahl_vier = wuerfelliste.count(4)
-    anzahl_fuenf = wuerfelliste.count(5)
-    anzahl_sechs = wuerfelliste.count(6)
-
-    kombinationen = [anzahl_eins, anzahl_zwei, anzahl_drei, anzahl_vier, anzahl_fuenf, anzahl_sechs]
-    
-    if 1 in wuerfelliste:
-        print("Super, du hast eine 1 gewürfelt, das gibt Punkte")
-        hatpunkte = True
-
-    if 5 in wuerfelliste:
-        print("Super, du hast eine 5 gewürfelt, das gibt Punkte")
-        hatpunkte = True
-
-    for index,anzahl in enumerate(kombinationen):
-        if anzahl >= 3:
-            print(f"Super, du hast eine Dreierkombination von {index+1} gewürfelt, das gibt Punkte")
-            hatpunkte = True
-
-    if not hatpunkte:
-        print("Leider hast du keine Punkte gewürfelt")
-    
-
-    else:
-        print("Gratuliere, du hast Punkte erreicht...")
-
-    return hatpunkte
-
-
-#Entwurf der Auswahl-Funktion der Würfel
-
-def auswahl(wuerfelliste):
-    wuerfelraus = []
-    was_entfernen = int(input("Welche Würfel willst du rausnehmen "))
-    wuerfelliste.remove(was_entfernen)
-    wuerfelraus.append(was_entfernen)
-    return wuerfelraus
-
-zwischenstand = auswahl(wuerfelliste)
-
-print(zwischenstand)
-print(wuerfelliste)
-
-
-
-def spielerwechsel():
-    pass #irgendwie müssen wir es schaffen, dass sich hier die Klasse ändert
-
-
-#def punkteermitteln
-#for i in wuerfelliste:
-    #if i == 1: return(100)
-
-# SPIEL
-
-print("\n")
-print("******** Herzlich willkommen zu Fraggles ********")
-print("\n")
-print("Bei Fraggles handelt es sich um ein Würfelspiel für Gross und Klein. Entscheide Dich im richtigen Moment fürs Risiko oder für die sichere Variante. ")
-print("Aber Achtung! --> Dieses Würfelspiel bietet Sucht- und auch ein erhebliches Frustrations-Potenzial! ;-D ")
-print("\n")
-print("Das Spiel eignet sich von 2 bis 6 Spieler")
-print("\n")
-
-#Funktion der Anzahl Spieler wird hier ausgeführt
-anzahl_spieler = spieler_eingabe()
-
-print("\n")
-print("Super, es werden in dieser Runde " + str(anzahl_spieler) + " Spieler mitmachen.")
-print("\n")
-print("Als nächstes kannst du die Namen von allen Spielern eingeben.")
-print("\n")
-
-#Hier erstellen wir die verschiedenen Speiler-Konten
-class Spieler:
-    def __init__(self, name):
-        self.name = name
-        self.punktestand_temp = 0
-        self.punktestand_total = 0
-
-spieler = []
-
-for i in range(anzahl_spieler):
-    neuer_spieler = Spieler(input(f"Bitte gib den Namen von Spieler {i+1} ein: "))
-    spieler.append(neuer_spieler)
-
-print("\n>")
-print("Sehr schön - jetzt wird gewürfelt!")
-print("\n>")
-
-wuerfelliste = wuerfeln()
-
-#Wenn ich Punkte gemacht haben, dann können wir nun unsere Würfel auswählen.
-if hatpunkte(wuerfelliste) == True:
-    auswahl()
-
-#Wenn wir keine Punkte gemacht haben, dann kommt der nächste Spieler an die Reihe
-else:
-    spielerwechsel()
-
-#Weiterspielen
-
-
-
-
-
-
-
-# Kombinierte Version - neu per 05.12.2022
-## Neue Funktionen, neuer Aufbau des Codes, etc.
-
-#Imports
-
-import random
-import time
-
-#Definition von unseren zentralen Funktionen für das Spiel
-
-
-##Funktion Spielereingabe
-def spieler_eingabe():
-    while True:
-        anzahl_spieler = input("Bitte Anzahl Spieler eingeben: ___")    
-        if anzahl_spieler >= "2" and anzahl_spieler <= "6" :
-            return int(anzahl_spieler)
-        else:
-            print("Die Anzahl Spieler muss zwischen 2 und 6 liegen")
-
-
-##Funktion Würfelwurf
+## Funktion Würfelwurf
 def wuerfeln(anzahlwuerfel = 6):
     janein = input("Möchtest du die Würfel würfeln? j/n \n")
     if janein == "j":
@@ -317,15 +24,14 @@ def wuerfeln(anzahlwuerfel = 6):
         print(wuerfelliste)
         return wuerfelliste
 
-    if janein == "n":
+    if janein == "n": #(Diese Funktion ist aktuell nicht umgesetzt)
         spielerwechsel()
 
-    
 
 
-##Funktion Punktevalidierung
-def hatpunkte(wuerfelliste):
-    hatpunkte = False
+## Funktion Punktevalidierung
+def hat_punkte(wuerfelliste):
+    hat_punkte = False
     
     anzahl_eins = wuerfelliste.count(1)
     anzahl_zwei = wuerfelliste.count(2)
@@ -338,69 +44,67 @@ def hatpunkte(wuerfelliste):
     
     if 1 in wuerfelliste:
         print("Super, du hast eine 1 gewürfelt, das gibt Punkte")
-        hatpunkte = True
+        hat_punkte = True
 
     if 5 in wuerfelliste:
         print("Super, du hast eine 5 gewürfelt, das gibt Punkte")
-        hatpunkte = True
+        hat_punkte = True
 
     for index,anzahl in enumerate(kombinationen):
         if anzahl >= 3:
             print(f"Super, du hast eine Dreierkombination von {index+1} gewürfelt, das gibt Punkte")
-            hatpunkte = True
+            hat_punkte = True
 
-    if not hatpunkte:
+    if not hat_punkte:
         print("Leider hast du keine Punkte gewürfelt")
     
 
     else:
         print("Gratuliere, du hast Punkte erreicht...")
 
-    return hatpunkte
+    return hat_punkte
 
 
-##Funktion Würfelauswahl
+## Funktion Würfelauswahl
 def auswahl(wuerfelliste):
-    wuerfelraus = []
+    wuerfel_raus = []
     nochmals = ("j")
     while nochmals == "j":
         was_entfernen = int(input("Welche Würfel willst du rausnehmen?\n"))
         wuerfelliste.remove(was_entfernen)
-        wuerfelraus.append(was_entfernen)
+        wuerfel_raus.append(was_entfernen)
         nochmals = input("Willst du weitere Würfel rausnehmen? j/n \n")
-    return wuerfelraus
+    return wuerfel_raus
 
 
 
-##Funktion Punkte aufschreiben
-def punkte(wuerfelraus):
+## Funktion Punkte aufschreiben
+def punkte(wuerfel_raus):
     zwischenpunktestand = 0
     
     for wurf in range(1,7):
-        if wuerfelraus.count(wurf) >= 3:
+        if wuerfel_raus.count(wurf) >= 3:
             if wurf == 1:
                 zwischenpunktestand += 1000
             else:
                 zwischenpunktestand += wurf *100
-        elif wuerfelraus.count(wurf) >= 1:
+        elif wuerfel_raus.count(wurf) >= 1:
             if wurf == 1:
-                zwischenpunktestand += 100 * wuerfelraus.count(wurf)
+                zwischenpunktestand += 100 * wuerfel_raus.count(wurf)
             if wurf == 5:
-                zwischenpunktestand += 50 * wuerfelraus.count(wurf)
+                zwischenpunktestand += 50 * wuerfel_raus.count(wurf)
 
     return zwischenpunktestand
 
 
-#Hier erstellen wir die verschiedenen Spieler-Konten
+## Hier erstellen wir die verschiedenen Spieler-Konten (aktuell nicht verwendet in der Spiel-Simulation)
+## Würde benötigt werden um die generierten Punkte pro Spielrunden den verschiedenen Spielern gutzuschreiben.
 class Spieler:
     def __init__(self, name):
         self.name = name
         self.punktestand_temp = 0
         self.punktestand_total = 0
 
-
-def spielerwechsel():
-    self.punktestand_total = self.punktestand_temp + zwischenpunktestand
     
 
 ## Funktion Gesamt-Spiel-Simulation
@@ -408,13 +112,13 @@ def main():
     print("\n")
     print("******** Herzlich willkommen zu Fraggles ********")
     print("\n")
-    print("Bei Fraggles handelt es sich um ein Würfelspiel für Gross und Klein. Entscheide Dich im richtigen Moment fürs Risiko oder für die sichere Variante. ")
+    print("Bei Fraggles handelt es sich um ein Würfelspiel für Gross und Klein.")
+    print("Entscheide Dich im richtigen Moment fürs Risiko oder für die sichere Variante.")
     print("Aber Achtung! --> Dieses Würfelspiel bietet Sucht- und auch ein erhebliches Frustrations-Potenzial! ;-D ")
     print("\n")
     print("Das Spiel eignet sich von 2 bis 6 Spieler")
     print("\n")
 
-    #Funktion der Anzahl Spieler wird hier ausgeführt
     anzahl_spieler = spieler_eingabe()
 
     print("\n")
@@ -422,7 +126,6 @@ def main():
     print("\n")
     print("Als nächstes kannst du die Namen von allen Spielern eingeben.")
     print("\n")
-
 
 
     spieler = []
@@ -437,24 +140,27 @@ def main():
 
     wuerfelliste = wuerfeln()
 
-    #Wenn ich Punkte gemacht haben, dann können wir nun unsere Würfel auswählen. Das wiederholt sich nun, bis alle Würfel rausgenommen wurden.
-    if hatpunkte(wuerfelliste) == True:
+    # Wenn ich Punkte gemacht haben, dann können wir nun unsere Würfel auswählen.
+    # Das wiederholt sich nun, bis alle Würfel rausgenommen wurden.
+    if hat_punkte(wuerfelliste) == True:
         zwischenstand = auswahl(wuerfelliste)
         print(f"Folgende Würfel hast du bereits herausgenommen " + str(zwischenstand))
-        print(f"Die folgenden Würfen hast du noch zur Auswahl " + str(wuerfelliste))
+        print(f"Die folgenden Würfel hast du noch zur Auswahl " + str(wuerfelliste))
 
         zwischenstand = punkte(zwischenstand)
         print(f"Du schreibst nun die folgenden Punkte auf: " + str(zwischenstand))
 
-    #Wenn wir keine Punkte gemacht haben, dann kommt der nächste Spieler an die Reihe    
-    else:
-        spielerwechsel()
+    # Wenn wir keine Punkte gemacht haben, dann kommt der nächste Spieler an die Reihe
+    # (Diese Funktion ist aktuell noch nicht umgesetzt)  
+    # else:
+        # spielerwechsel()
 
     # Wir wollen erneut würfeln
     wuerfelliste = wuerfeln(len(wuerfelliste))
 
 
-#Ausführung Spielsimulation
+
+# Ausführung Spielsimulation mit Main
 if __name__ == "__main__":
     main()
 
