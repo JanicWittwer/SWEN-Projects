6584f2cbc421ff34bac9ece780cbee6dce29bf80
JanicWittwer <92203361+JanicWittwer@users.noreply.github.com>
2022-12-17 12:53:32 +0100

    Update test_unittests.py

--- test_unittests.py
+++ test_unittests.py
@@ -1,4 +1,7 @@
 
+# Wir haben versucht, die Unittests für unsere Funktionen aufzubauen. Das dazu notwendige Testfile haben wir erstellt.
+# Bei der Erstellung der Testparameter für unsere Funktionen sind wir aber leider gescheitert, obwohl wir viel Zeit investiert haben :-(
+
 import unittest
 from Wuerfelspiel_Fraggles import spieler_eingabe
 
@@ -6,10 +9,14 @@ class Test1(unittest.TestCase):
     def test_1(self):
         self.assertTrue(spieler_eingabe)
 
-#assertEqual --> Um ein erwartetes Ergebnis zu prüfen.
-#assertTrue --> Um eine Bedingung zu verifizieren.
-#assertFalse --> Um eine Bedingung zu verifizieren.
-#assertRises --> Um zu prüfen, ob eine bestimmte Ausnahme ausgelöst wird.
-
+class Test2(unittest.TestCase):
+    def test_2(self):
+        self.assertEqual(4, 4)
+        
 if __name__ == '__main__':
     unittest.main()
+
+#assertEqual --> Testmethode, um ein erwartetes Ergebnis zu prüfen.
+#assertTrue --> Testmethode, um eine Bedingung zu verifizieren.
+#assertFalse --> Testmethode, um eine Bedingung zu verifizieren.
+#assertRises --> Testmethode, um zu prüfen, ob eine bestimmte Ausnahme ausgelöst wird.
