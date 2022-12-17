befea8426f8dfee5a0158c03548e36668ddc01e4
JanicWittwer <92203361+JanicWittwer@users.noreply.github.com>
2022-12-17 12:44:56 +0100

    Update test_unittests.py

--- test_unittests.py
+++ test_unittests.py
@@ -1,1 +1,15 @@
 
+import unittest
+from Wuerfelspiel_Fraggles import spieler_eingabe
+
+class Test1(unittest.TestCase):
+    def test_1(self):
+        self.assertTrue(spieler_eingabe)
+
+#assertEqual --> Um ein erwartetes Ergebnis zu prüfen.
+#assertTrue --> Um eine Bedingung zu verifizieren.
+#assertFalse --> Um eine Bedingung zu verifizieren.
+#assertRises --> Um zu prüfen, ob eine bestimmte Ausnahme ausgelöst wird.
+
+if __name__ == '__main__':
+    unittest.main()
